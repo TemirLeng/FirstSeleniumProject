22d90b742f3ad2d2ef5f97e2aebaa05c722382c8
chynar iskenderova <chi93@chynars-MacBook-Air.local>
2019-05-30 18:40:14 -0500

    I add WebElement and changed the website

--- src/com/cbt/SeleniumDemo.java
+++ src/com/cbt/SeleniumDemo.java
@@ -1,12 +1,16 @@
 package com.cbt;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumDemo {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver","C:/Users/darin/Documents/selenium dependencies/drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://www.cybertek.com");
+		driver.get("https://techtorialacademy.com/");
+	WebElement name=driver.findElement(By.id("//h1[@itemprop='name']"));
+	System.out.println(name.getText());
 		
 	}
 
